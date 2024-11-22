3384e1ba882a61ee0d0c0e1f6915c23b3ca816e3
Izabellyilva <00001114204791sp@al.educacao.sp.gov.br>
2024-11-22 15:48:45 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Filosofia',
+    'Qual a teoria de aristóteles?',
+    ' Segundo Aristóteles, a filosofia é essencialmente teorética: deve decifrar o enigma do universo, em face do qual a atitude inicial do espírito é o assombro do mistério. O seu problema fundamental é o problema do ser, não o problema da vida.'
+)
+
+criaCartao(
+    'Filosofia',
+    'Quais são os três princípios de Aristóteles?',
+    '1- Princípio de Identidade: A é A; 2- Princípio de não contradição: é impossível A é A e não-A ao mesmo tempo; 3- Princípio do terceiro excluído: A é x ou não-x, não há terceira possibilidade.'
+)
+
+criaCartao(
+    'Filosofia',
+    'O que é Deus para Aristóteles?',
+    ' é o "primeiro motor" ao qual necessariamente se filiava a cadeia de todos os movimentos, pois tudo o que se move é movido por outra coisa.'
+)
+
+criaCartao(
+    'Filosofia',
+    'O que Aristóteles acreditava?',
+    ' Aristóteles acreditava que educar para a virtude era também um modo de educar para viver bem – e isso queria dizer, entre outras coisas, viver uma vida prazerosa.'
+)
